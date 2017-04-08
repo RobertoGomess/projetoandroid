29bc28b360e38197dea8344e90eb4282f2f660e2
Hyago Melo <hyagocontato.m@gmail.com>
2017-04-08 14:43:49 -0300

    alterei alguns icons, e terminei as aparencias das telas, só falta eu instanciar os componentes da tela, a noite quando eu entrar eu termino dá uma olhada lá no facebook na mensagem que deixei e baixa sempre os icones cinza claro seta os 36dp por padrão. só tirei alguns porque achei uns mais bonitin mais se quiser mudar dnv depois pd ficar a vontade.

--- MeuProntuario/app/src/main/java/br/com/meuprontuario/meuprontuario/CadastroActivity.java
+++ MeuProntuario/app/src/main/java/br/com/meuprontuario/meuprontuario/CadastroActivity.java
@@ -1,7 +1,10 @@
 package br.com.meuprontuario.meuprontuario;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CadastroActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class CadastroActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro);
+
+        Button btnCadastrarPasso2 = (Button)findViewById(R.id.button_cadastrar);
+
+        btnCadastrarPasso2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cadastroPasso2 = new Intent(CadastroActivity.this,CadastroPasso2Activity.class);
+                startActivity(cadastroPasso2);
+            }
+        });
     }
 }
