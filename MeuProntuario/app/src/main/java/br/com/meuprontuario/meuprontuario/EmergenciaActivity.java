2846a409f394806faaf13fa863022dbf7744a110
Hyago Melo <hyagocontato.m@gmail.com>
2017-04-13 01:54:41 -0300

    Criei as telas emergencia e perfil, a de emergencia tá sen nada porque tem que colocar o maps, e eu removi a opção prontuário (atendimento) porque não era útil já que quem vai usar esse app é só o paciente e na tela de perfil o que vai mostrar seriam as mesmas coisas do prontuário.

--- MeuProntuario/app/src/main/java/br/com/meuprontuario/meuprontuario/EmergenciaActivity.java
+++ MeuProntuario/app/src/main/java/br/com/meuprontuario/meuprontuario/EmergenciaActivity.java
@@ -1,16 +1,14 @@
 package br.com.meuprontuario.meuprontuario;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.TextView;
 
-public class HomeActivity extends AppCompatActivity {
+public class EmergenciaActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
     private Intent tela;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -19,18 +17,17 @@ public class HomeActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    // já vai estar na tela
+                    tela = new Intent(EmergenciaActivity.this,HomeActivity.class);
+                    startActivity(tela);
                     return true;
                 case R.id.navigation_emergencia:
-                    tela = new Intent(HomeActivity.this,EmergenciaActivity.class);
-                    startActivity(tela);
+                    // já vai estar na tela
                     return true;
                 /*case R.id.navigation_prontuario:
                     mTextMessage.setText(R.string.title_prontuario);
                     return true;*/
                 case R.id.navigation_perfil:
-                    tela = new Intent(HomeActivity.this,PerfilActivity.class);
-                    startActivity(tela);
+                    tela = new Intent(EmergenciaActivity.this,PerfilActivity.class);
                     return true;
             }
             return false;
@@ -41,11 +38,6 @@ public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
-
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        setContentView(R.layout.activity_emergencia);
     }
-
 }
