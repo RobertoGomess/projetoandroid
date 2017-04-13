566d46054eacf78e3eb1361db3d50b43aabaf41f
Hyago Melo <hyagocontato.m@gmail.com>
2017-04-13 11:42:32 -0300

    instanciei alguns itens das telas

--- MeuProntuario/app/src/main/java/br/com/meuprontuario/meuprontuario/PerfilActivity.java
+++ MeuProntuario/app/src/main/java/br/com/meuprontuario/meuprontuario/PerfilActivity.java
@@ -7,10 +7,18 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class PerfilActivity extends AppCompatActivity {
 
     private Intent tela;
+    private TextView sexo;
+    private TextView nome;
+    private TextView data;
+
+
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -18,11 +26,11 @@ public class PerfilActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    tela = new Intent(PerfilActivity.this,HomeActivity.class);
+                    tela = new Intent(PerfilActivity.this, HomeActivity.class);
                     startActivity(tela);
                     return true;
                 case R.id.navigation_emergencia:
-                    tela = new Intent(PerfilActivity.this,EmergenciaActivity.class);
+                    tela = new Intent(PerfilActivity.this, EmergenciaActivity.class);
                     startActivity(tela);
                     return true;
                 /*case R.id.navigation_prontuario:
@@ -41,6 +49,11 @@ public class PerfilActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
+
+
+        sexo = (TextView) findViewById(R.id.textView_sexo);
+        nome = (TextView) findViewById(R.id.textView_nome);
+        data = (TextView) findViewById(R.id.textView_datanascimento);
 
     }
 }
