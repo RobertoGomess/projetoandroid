57866b1777213f4b27a78b093e13942515659b42
Beto junior <betojunior1250@hotmail.com>
2017-04-14 20:02:50 -0300

    1 - google map adcionado na tele emergencia. depois teste ai man

--- MeuProntuario/app/src/main/java/br/com/meuprontuario/meuprontuario/EmergenciaFragment.java
+++ MeuProntuario/app/src/main/java/br/com/meuprontuario/meuprontuario/EmergenciaFragment.java
@@ -1,49 +1,88 @@
 package br.com.meuprontuario.meuprontuario;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.net.Uri;
 import android.net.sip.SipSession;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiActivity;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 /**
  * Created by betoj on 13/04/2017.
  */
 
-public class EmergenciaFragment extends Fragment {
+public class EmergenciaFragment extends Fragment implements OnMapReadyCallback {
+
     private FloatingActionButton btnLigar;
+    private GoogleApiClient googleApiClient;
+
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,  Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        View view = inflater.inflate(R.layout.fragment_emergencia,container,false);
-        btnLigar = (FloatingActionButton)view.findViewById(R.id.floatingActionButtonLigar);
+
+        View view = inflater.inflate(R.layout.fragment_emergencia, container, false);
+        btnLigar = (FloatingActionButton) view.findViewById(R.id.floatingActionButtonLigar);
         btnLigar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                try{
+                try {
                     Uri uri = Uri.parse("tel:192");
-                    Intent i = new Intent(Intent.ACTION_DIAL,uri);
+                    Intent i = new Intent(Intent.ACTION_DIAL, uri);
                     startActivity(i);
-                }catch (Exception e){
-                    Toast.makeText(getContext(),"Não foi possível realizar a ligação. "+e.getMessage(),Toast.LENGTH_LONG).show();
+                } catch (Exception e) {
+                    Toast.makeText(getContext(), "Não foi possível realizar a ligação. " + e.getMessage(), Toast.LENGTH_LONG).show();
                 }
             }
         });
 
+        setTargetFragment(getTargetFragment(), R.layout.fragment_emergencia);
+        // Get the SupportMapFragment and request notification
+        // when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) this.getChildFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
         return view;
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+       googleMap.setMapType(googleMap.MAP_TYPE_NORMAL);
+       googleMap.getUiSettings().setZoomControlsEnabled(true);
+    }
 
 }
 
--- MeuProntuario/app/src/main/java/br/com/meuprontuario/meuprontuario/HomeActivity.java
+++ MeuProntuario/app/src/main/java/br/com/meuprontuario/meuprontuario/HomeActivity.java
@@ -1,5 +1,6 @@
 package br.com.meuprontuario.meuprontuario;
 
+import android.support.v4.app.Fragment;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
@@ -18,6 +19,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
+
+
+import com.google.android.gms.maps.SupportMapFragment;
 
 import java.util.ArrayList;
 import java.util.List;
