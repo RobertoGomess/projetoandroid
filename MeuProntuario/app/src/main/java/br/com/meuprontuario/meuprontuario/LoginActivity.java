fabc2aa698fe84dda4ecd5aa1151b90ea3f8f00e
Beto Junior <betojunior1250@hotmail.com>
2017-06-17 19:42:32 -0300

    tentativa de enviar um POST

--- MeuProntuario/app/src/main/java/br/com/meuprontuario/meuprontuario/LoginActivity.java
+++ MeuProntuario/app/src/main/java/br/com/meuprontuario/meuprontuario/LoginActivity.java
@@ -1,15 +1,31 @@
 package br.com.meuprontuario.meuprontuario;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.animation.Interpolator;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONStringer;
+
+import java.util.concurrent.ExecutionException;
+
+import br.com.meuprontuario.meuprontuario.PacoteReceita.Receita;
+import br.com.meuprontuario.meuprontuario.PacoteReceita.ReceitasActivity;
+import br.com.meuprontuario.meuprontuario.PacoteReceita.ReceitasFragment;
 
 /**
  * Created by betoj on 07/04/2017.
@@ -28,7 +44,10 @@ public class LoginActivity extends AppCompatActivity {
 
     //declaração variaveis
 
+    ProgressDialog progressDialog;
+    Context context = this;
     boolean login;
+    Paciente paciente = new Paciente();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +55,7 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         //instanciando elementos
+
         txtEmail = (EditText)findViewById(R.id.txtNomeUsuario);
         txtSenha = (EditText)findViewById(R.id.txtConfirmaSenha);
         switchLembraSenha = (Switch)findViewById(R.id.switchLembraSenha);
@@ -70,20 +90,121 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String email = txtEmail.getText().toString();
                 String senha = txtSenha.getText().toString();
-                    //code condicao...
-                    //condicao para chamar a outra tela...
-                    login=true;
+
+                progressDialog = ProgressDialog.show(LoginActivity.this, "Aguarde", "Baixando informações...");
+
+
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            lerJson();
+                            progressDialog.dismiss();
+
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    progressDialog.dismiss();
+                                    Toast.makeText(context,paciente.getNome(),Toast.LENGTH_LONG).show();
+
+                                    /*ReceitasFragment fragment = (ReceitasFragment) getSupportFragmentManager().findFragmentByTag("mainFrag");
+                                    if (fragment==null){
+                                        fragment = new ReceitasFragment();
+                                        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+                                        ft.replace(R.id.rl_frag_receita, fragment , "mainFrag");
+                                        ft.commit();
+                                    }*/
+                                }
+                            });
+
+
+                        } catch (ExecutionException e) {
+                            e.printStackTrace();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }).start();
+
+
+
+                //code condicao...
+                //condicao para chamar a outra tela...
+                    /*login=true;
                     if (login==true){
                         Intent home = new Intent(LoginActivity.this,HomeActivity.class);
                         //abre a tela home
                         startActivity(home);
                         //finaliza a tela atual
                         finish();
-                }
+                }*/
 
             }
         });
 
+
+    }
+    public void lerJson() throws ExecutionException, InterruptedException, JSONException {
+
+        //se comunicando com o servidor
+        BaseRequester baseRequester = new BaseRequester();
+        baseRequester.setUrl("http://www.betojunior.com.br/webservice/PacotePaciente/pacienteLogin.php");
+        baseRequester.setContext(context);
+
+        //Caso precise enviar dados para o servidor, utilize o jsonput
+        Paciente filtro = new Paciente();
+        filtro.setEmail(txtEmail.getText().toString());
+        filtro.setSenha(txtSenha.getText().toString());
+        //Toast.makeText(context,filtro.getEmail().toString(),Toast.LENGTH_LONG).show();
+
+
+        JSONObject jsonPut = new JSONObject();
+        jsonPut.put("email", filtro.getEmail());
+        jsonPut.put("senha", filtro.getSenha());
+
+        baseRequester.setJsonString(jsonPut.toString());
+
+        //recuperando o json em formato tetxo
+        String jsonString = baseRequester.execute(baseRequester).get();
+
+        //transformando o json texto em JsonObject
+        JSONObject jsonObject = new JSONObject(jsonString);
+
+        paciente.setId(Integer.parseInt(jsonObject.get("id").toString()));
+        paciente.setNome(jsonObject.get("nome").toString());
+        paciente.setSexo(Integer.parseInt(jsonObject.get("sexo").toString()));
+        paciente.setTelefone(jsonObject.getString("telefone").toString());
+        paciente.setCpf(jsonObject.getString("cpf").toString());
+        paciente.setSus(Integer.parseInt(jsonObject.get("sus").toString()));
+        paciente.setSenha(jsonObject.getString("senha").toString());
+        paciente.setEmail(jsonObject.getString("email").toString());
+        paciente.setEndereco_id(Integer.parseInt(jsonObject.getString("endereco_id").toString()));
+
+
+        /*paciente.setId(Integer.parseInt(jsonObject.get("").toString()));
+        paciente.setId(Integer.parseInt(jsonObject.get("").toString()));
+        paciente.setId(Integer.parseInt(jsonObject.get("").toString()));
+        paciente.setId(Integer.parseInt(jsonObject.get("").toString()));*/
+
+        //transformando o json texto em JsonOArray
+        //JSONArray jsonArrayUsers = new JSONArray(jsonString);
+
+        /*for (int i=0; i < jsonArrayUsers.length(); i++) {
+
+            JSONObject jsonObjectUsuario = jsonArrayUsers.getJSONObject(i);
+
+            Receita receita = new Receita();
+
+            receita.setId(Integer.parseInt(jsonObjectUsuario.get("id").toString()));
+            receita.setData(jsonObjectUsuario.getString("data").toString());
+            receita.setValidade(jsonObjectUsuario.getString("validade").toString());
+            receita.setDoenca(jsonObjectUsuario.getString("doenca").toString());
+            receita.setDescricao(jsonObjectUsuario.getString("descricao").toString());
+
+            listReceita.add(receita);
+        }*/
 
     }
 }
---
+++ MeuProntuario/app/src/main/java/br/com/meuprontuario/meuprontuario/Paciente.java
@@ -0,0 +1,152 @@
+package br.com.meuprontuario.meuprontuario;
+
+/**
+ * Created by Beto on 17/06/2017.
+ */
+
+public class Paciente {
+    private int id;
+    private String nome;
+    private String telefone;
+    private String cpf;
+    private long sus;
+    private int sexo;
+    private String usuario;
+    private String senha;
+    private String email;
+    private int endereco_id;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the telefone
+     */
+    public String getTelefone() {
+        return telefone;
+    }
+
+    /**
+     * @param telefone the telefone to set
+     */
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    /**
+     * @return the cpf
+     */
+    public String getCpf() {
+        return cpf;
+    }
+
+    /**
+     * @param cpf the cpf to set
+     */
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    /**
+     * @return the sus
+     */
+    public long getSus() {
+        return sus;
+    }
+
+    /**
+     * @param sus the sus to set
+     */
+    public void setSus(long sus) {
+        this.sus = sus;
+    }
+
+    /**
+     * @return the sexo
+     */
+    public int getSexo() {
+        return sexo;
+    }
+
+    /**
+     * @param sexo the sexo to set
+     */
+    public void setSexo(int sexo) {
+        this.sexo = sexo;
+    }
+
+    /**
+     * @return the usuario
+     */
+    public String getUsuario() {
+        return usuario;
+    }
+
+    /**
+     * @param usuario the usuario to set
+     */
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    /**
+     * @return the senha
+     */
+    public String getSenha() {
+        return senha;
+    }
+
+    /**
+     * @param senha the senha to set
+     */
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getEndereco_id() {
+        return endereco_id;
+    }
+
+    public void setEndereco_id(int endereco_id) {
+        this.endereco_id = endereco_id;
+    }
+}
